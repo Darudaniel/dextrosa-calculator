aa0ec435e937d809b8d311e5bee9e9ba28c8104c
darudaniel <danielelrincon@gmail.com>
2020-12-15 10:30:35 -0500

    code cleaned

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
-const FM_IDEAL = 9
+//constants
 const MINUTOS_DIA = 1440
-
-const mgDextrosaAl5 = 5000
-const mgDextrosaAl10 = 10000
-const mgDextrosaAl30 = 30000
-const mgDextrosaAl50 = 50000
-const cien = 100
-
-const botonCalcular =  document.getElementById('botonCalcular')
-
+const MG_DEXTROSA_AL_5 = 5000
+const MG_DEXTROSA_AL_10 = 10000
+const MG_DEXTROSA_AL_30 = 30000
+const MG_DEXTROSA_AL_50 = 50000
+const CIEN = 100
+//variables
+let fmIdeal = 9
+let peso
+let dextrosaNecesaria
+let cantidadDextrosaAl5Porciento
+let cantidadDextrosaAl10Porciento
+let cantidadDextrosaAl30Porciento
+let cantidadDextrosaAl50Porciento
+let diasDeNacido
 let dextrosaSeleccionada
 let dextrosaSeleccionadaEnCc
-let dextrosaNecesaria
 let resultadoResta
+let porcentajeDeDextrosa
 let natrolEnCc
 let meqNatrol
 let katrolEnCC
 let electrolitosMostrados
-
+let usoCateter
+//elements
+const botonCalcular =  document.getElementById('botonCalcular')
+//app init
 const initializer = () => {
+  //when you click on "calcular"
   botonCalcular.onclick = () => {
-    let peso = document.getElementById("peso").value
-    dextrosaNecesaria = FM_IDEAL * MINUTOS_DIA * peso
-    let cantidadDextrosaAl5Porciento = (dextrosaNecesaria * cien) / mgDextrosaAl5
-    let cantidadDextrosaAl10Porciento = (dextrosaNecesaria * cien) / mgDextrosaAl10
-    let cantidadDextrosaAl30Porciento = (dextrosaNecesaria * cien) / mgDextrosaAl30
-    let cantidadDextrosaAl50Porciento = (dextrosaNecesaria * cien) / mgDextrosaAl50
-    
-    let diasDeNacido = document.getElementById("dias").value
-    
-    let cantidadDeLiquidos = (dias) => {
-      // let diasArgument = dias
+    // get the data of patient
+    peso = document.getElementById("peso").value
+    diasDeNacido = document.getElementById("dias").value
+    // and make the calculations with this data
+
+    //LIQUIDS CALCULATIONS
+    const cantidadDeLiquidos = (dias) => {
       switch(parseInt(dias)) {
         case 1:
-          return peso * 70      
+          return peso * 70
           break
         case 2:
           return peso * 80
@@ -50,21 +55,19 @@ const initializer = () => {
         default:
           console.log('el campo esta vacio')
           break
-      }    
+      }
     }
 
-    const objetivo5 = document.getElementById('resultado5')
-    objetivo5.innerHTML = `-Dextrosa al 5 en cc :---${cantidadDextrosaAl5Porciento.toFixed(1)} CC`
-    const objetivo10 = document.getElementById('resultado10')
-    objetivo10.innerHTML = `-Dextrosa al 10 en cc :---${cantidadDextrosaAl10Porciento.toFixed(1)} CC`
-    const objetivo30 = document.getElementById('resultado30')
-    objetivo30.innerHTML = `-Dextrosa al 30 en cc :---${cantidadDextrosaAl30Porciento.toFixed(1)} CC`
-    const objetivo50 = document.getElementById('resultado50')
-    objetivo50.innerHTML = `-Dextrosa al 50 en cc :---${cantidadDextrosaAl50Porciento.toFixed(1)} CC`
-
-    
-    
-    let seleccionarDextrosa = (dex) => {
+    //DEXTROSE CALCULATIONS
+    //calculates how much dextrose the patient needs
+    dextrosaNecesaria = fmIdeal * MINUTOS_DIA * peso
+    //calulates the amount of dextrosa in CC based on each concentration
+    cantidadDextrosaAl5Porciento = (dextrosaNecesaria * CIEN) / MG_DEXTROSA_AL_5
+    cantidadDextrosaAl10Porciento = (dextrosaNecesaria * CIEN) / MG_DEXTROSA_AL_10
+    cantidadDextrosaAl30Porciento = (dextrosaNecesaria * CIEN) / MG_DEXTROSA_AL_30
+    cantidadDextrosaAl50Porciento = (dextrosaNecesaria * CIEN) / MG_DEXTROSA_AL_50
+    //selects the concentration of dextrose that the patient needs
+    const seleccionarDextrosa = (dex) => {
       if(dex > cantidadDextrosaAl5Porciento) {
         dextrosaSeleccionada = "Dextrosa al 5%"
         dextrosaSeleccionadaEnCc = cantidadDextrosaAl5Porciento
@@ -81,56 +84,35 @@ const initializer = () => {
         console.log('no se pudo seleccionar una dextrosa')
       }
     }
-
     seleccionarDextrosa(cantidadDeLiquidos(diasDeNacido))
-
-    const objetivoLiquidos = document.getElementById('liquidosNecesarios')
-    // objetivoLiquidos.innerHTML = `-Administrar ${cantidadDeLiquidos(diasDeNacido)} CC de liquidos en total.<br>De los cuales ${dextrosaSeleccionadaEnCc} CC deben ser de ${dextrosaSeleccionada}`
+    //calculates how much saline solution you need to increase to dextrose to meet the requirements
     const restar = () => {
       resultadoResta = cantidadDeLiquidos(diasDeNacido) - dextrosaSeleccionadaEnCc
     }
     restar()
-    objetivoLiquidos.innerHTML = `-Administrar ${dextrosaSeleccionadaEnCc.toFixed(1)} CC de ${dextrosaSeleccionada}<br> y agregar ${resultadoResta.toFixed(1)} CC de solucion salina.`
+    //calculates which is the final concentration of solution
+    porcentajeDeDextrosa = ((dextrosaNecesaria / 1000) * CIEN) / cantidadDeLiquidos(diasDeNacido)
 
-    // const objetivoDextrosaAdecuada = document.getElementById('dextrosaAdecuada')
-    // objetivoDextrosaAdecuada.innerHTML = `-Debes usar ${dextrosaSeleccionada}` 
-
-    let porcentajeDeDextrosa = ((dextrosaNecesaria / 1000) * cien) / cantidadDeLiquidos(diasDeNacido)
-
-    const objetivoConcentracion = document.getElementById('concentracion')
-    objetivoConcentracion.innerHTML = `(La solución tendra una concentracion de ${porcentajeDeDextrosa.toFixed(1)}% dextrosa)`
-
-
+    //ELECTROLITS CALCULATE
+    //validates if the patient needs electrolits
     const validarElectrolitos = () => {
       if(parseFloat(diasDeNacido) >= 3) {
         meqNatrol = peso * 3
         natrolEnCc = (meqNatrol * 10) / 20
         katrolEnCC = natrolEnCc / 2
+        electrolitosMostrados = `-Agregar ${natrolEnCc.toFixed(1)} CC de Natrol <br> y ${katrolEnCC.toFixed(1)} CC de Katrol`
       } else if(parseFloat(diasDeNacido) >= 2){
         meqNatrol = peso * 3
         natrolEnCc = (meqNatrol * 10) / 20
-      } else {
-        console.log('no se deben agregar electrolitos')
-      }
-    }
-    validarElectrolitos()
-    const mostrarElectrolitos = () => {
-      if(parseFloat(diasDeNacido) >= 3) {
-        electrolitosMostrados = `-Agregar ${natrolEnCc.toFixed(1)} CC de Natrol <br> y ${katrolEnCC.toFixed(1)} CC de Katrol`
-      } else if(parseFloat(diasDeNacido) >= 2) {
         electrolitosMostrados = `-Agregar ${natrolEnCc.toFixed(1)} CC de Natrol`
       } else {
         electrolitosMostrados = `-No se agregan electrolitos`
       }
     }
-    mostrarElectrolitos()
+    validarElectrolitos()
 
-    const objetivoElectrolitos = document.getElementById('electrolitos')
-    // objetivoElectrolitos.innerHTML = `Agregar ${natrolEnCc} cc de natrol y ${katrolEnCC} cc de katrol`
-    objetivoElectrolitos.innerHTML = electrolitosMostrados
-    
-    let usoCateter
-    let validacionCateter = () => {
+    //CATETER'S USE DETERMINATE    
+    const validacionCateter = () => {
       if(porcentajeDeDextrosa > 12.5) {
         usoCateter = "Debido a la concentracion debe utilizarse cateter central"
       } else {
@@ -138,9 +120,25 @@ const initializer = () => {
       }
     }
     validacionCateter()
+
+    //SHOW THE RESULTS IN HTML
+    const objetivo5 = document.getElementById('resultado5')
+    objetivo5.innerHTML = `-Dextrosa al 5 en cc :---${cantidadDextrosaAl5Porciento.toFixed(1)} CC`
+    const objetivo10 = document.getElementById('resultado10')
+    objetivo10.innerHTML = `-Dextrosa al 10 en cc :---${cantidadDextrosaAl10Porciento.toFixed(1)} CC`
+    const objetivo30 = document.getElementById('resultado30')
+    objetivo30.innerHTML = `-Dextrosa al 30 en cc :---${cantidadDextrosaAl30Porciento.toFixed(1)} CC`
+    const objetivo50 = document.getElementById('resultado50')
+    objetivo50.innerHTML = `-Dextrosa al 50 en cc :---${cantidadDextrosaAl50Porciento.toFixed(1)} CC`
+    const objetivoLiquidos = document.getElementById('liquidosNecesarios')
+    objetivoLiquidos.innerHTML = `-Administrar ${dextrosaSeleccionadaEnCc.toFixed(1)} CC de ${dextrosaSeleccionada}<br> y agregar ${resultadoResta.toFixed(1)} CC de solucion salina.`
+    const objetivoConcentracion = document.getElementById('concentracion')
+    objetivoConcentracion.innerHTML = `(La solución tendra una concentracion de ${porcentajeDeDextrosa.toFixed(1)}% dextrosa)`
+    const objetivoElectrolitos = document.getElementById('electrolitos')    
+    objetivoElectrolitos.innerHTML = electrolitosMostrados
     const objetivoCateter = document.getElementById('cateterCentral')
     objetivoCateter.innerHTML = usoCateter
-  }  
+  }
 }
 
 initializer()
